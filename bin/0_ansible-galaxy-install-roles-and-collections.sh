144e30825dd9596e9e1b6e9f3c1f3ae58e91673a
Gerald E. Bennett <GerryBennett@texasmutual.com>
2021-09-19 15:04:04 +0000

    Script to install ansible roles and collections

---
+++ bin/0_ansible-galaxy-install-roles-and-collections.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROG_PATH=${BASH_SOURCE[0]}      # this script's name
+PROG_NAME=${PROG_PATH##*/}       # basename of script (strip path)
+PROG_DIR="$(cd "$(dirname "${PROG_PATH:-$PWD}")" 2>/dev/null 1>&2 && pwd)"
+APP_DIR="$(cd "${PROG_DIR}/.." 2>/dev/null 1>&2 && pwd)"
+
+ansible-galaxy role install  ${APP_DIR}/requirements.yml
+ansible-galaxy collection install ${APP_DIR}/requirements.yml
