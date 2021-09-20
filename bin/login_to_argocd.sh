8eed8ac22bcf63e9ce7c95de8d39ec633029884a
Gerald E. Bennett <GerryBennett@texasmutual.com>
2021-09-20 13:21:22 +0000

    additional scripts

--- bin/login_to_argocd.sh
+++ bin/login_to_argocd.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+PROG_PATH=${BASH_SOURCE[0]}      # this script's name
+PROG_NAME=${PROG_PATH##*/}       # basename of script (strip path)
+PROG_DIR="$(cd "$(dirname "${PROG_PATH:-$PWD}")" 2>/dev/null 1>&2 && pwd)"
+APP_DIR="$(cd "${PROG_DIR}/.." 2>/dev/null 1>&2 && pwd)"
 
 declare CLUSTER=agocd.local
 declare USERNAME=admin
